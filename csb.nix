d8a795cc26716e2c61f234ae50aa3fdbf1f6f93f
Dom <dominique.quevy@gmail.com>
2023-03-24 19:56:28 +0000

    tuning

--- csb.nix
+++ csb.nix
@@ -4,15 +4,17 @@ stdenv.mkDerivation {
     name = "csb";
     buildInputs = [
         flutter 
-        chromium
+        # chromium
     ];
     shellHook = ''
         export ROOT_FLUTTER="/nix/store/gxjjq4mlvcpqaxj3sfvy7gnv2pwqs1ri-flutter-3.3.8"
         export ROOT_DART_SDK="/nix/store/gxjjq4mlvcpqaxj3sfvy7gnv2pwqs1ri-flutter-3.3.8/bin/cache/dart-sdk"
         export ROOT_CHROMIUM="/nix/store/1wd0wdakqll127nf87ldbvpxppi7sry5-chromium-111.0.5563.64"
         export CHROME_EXECUTABLE="$ROOT_CHROMIUM/bin/chromium-browser"
-        export ROOT_PROJECT="$HOME/workspace/flutter_project"
+        export ROOT_USER="/project/home/dominique-quevy/workspace"
+        export ROOT_PROJECT="$ROOT_USER/flutter_project"
         export LIB_PROJECT="$ROOT_PROJECT/lib"
-        export PATH="$PWD:$ROOT_FLUTTER/bin:$ROOT_DART_SDK/bin:$CHROME_EXECUTABLE:$ROOT_PROJECT:$PATH"
+        export BUILD_PROJECT="$ROOT_PROJECT/build"
+        export PATH="$ROOT_FLUTTER/bin:$ROOT_DART_SDK/bin:$ROOT_PROJECT:$ROOT_USER:$PATH"
     '';
 }
